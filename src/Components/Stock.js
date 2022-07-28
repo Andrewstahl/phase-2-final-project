ca9dab4b2e5295c9a461f93d309c86b52c1333eb
Andrew Stahl <andrewstahl96@gmail.com>
2022-07-28 19:55:13 -0400

    StockList, Stock and db.json all working properly

--- src/Components/Stock.js
+++ src/Components/Stock.js
@@ -3,18 +3,19 @@ import React from "react";
 export default function Stock({ stock }) {
 const {name, holdings, favorite} = stock;
 
-  let totalStocks;
+  let totalStocks = 0;
   const holdingsLength = holdings.length;
-  console.log(holdings, holdingsLength)
-  for (let i = holdingsLength - 1; i > 0; i--) {
-    // console.log(holdings[i]);
+  // console.log(holdingsLength -1)
+  for (let i = holdingsLength - 1; i > -1; i--) {
+    totalStocks += holdings[i].amount;
+    // console.log(totalStocks)
   }
 
   return (
     <div>
       <h4>{name}</h4>
-      <p>{totalStocks}</p>
-      <p>Favorite - {favorite}</p>
+      <p>{totalStocks} Shares</p>
+      <p>{favorite ? "Liked" : "Not Liked"}</p>
     </div>
   )
 }
