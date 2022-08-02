cfc81dedc884a0a7a7f27b43f26d5954e20c321f
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-02 17:50:23 -0400

    Changed header and some formatting pieces

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="red-line">
-      <h1>Header Placeholder</h1>
+    <div className="redLine">
+      <h1>Stahl Stock Trader</h1>
     </div>
   )
 }
