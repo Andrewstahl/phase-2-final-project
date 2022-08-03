273ca18908d619bbbb6fcfb2db31e0d676828ba4
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-03 06:37:29 -0400

    Added styling to stock elements on the page

--- src/Components/StockList.js
+++ src/Components/StockList.js
@@ -3,7 +3,11 @@ import Stock from "./Stock";
 
 export default function StockList({ stocks }) {
   const stockElements = stocks.map(stock => {
-    return <Stock key={stock.id} stock={stock} />
+    return (
+      <div className="stockElement">
+        <Stock key={stock.id} stock={stock} />
+      </div>
+    )
   })
 
   return (
