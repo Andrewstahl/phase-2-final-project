e6a2f302642840d2f5e81e2a2d82dfe8cad52656
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-04 18:25:23 -0400

    Updated formatting for NavLinks when selected

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,7 +9,6 @@ const linkStyles = {
   // padding: "12px",
   margin: "10px",
   fontSize: "20px",
-  // background: "blue",
   textDecoration: "none",
   color: "#171785",
   fontWeight: "bold",
@@ -17,40 +16,50 @@ const linkStyles = {
   alignItems: "center"
 };
 
+const linkStylesActive = {
+  ...linkStyles,
+  background: "white"
+}
+
 export default function NavBar() {
   return (
-    <div style={{ display: "flex" }}>
-      {/* <h3>NavBar Placeholder</h3> */}
-      <NavLink 
-        to="/"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink 
-        to="/favorites"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Favorites
-      </NavLink>
-      <NavLink 
-        to="/allocations"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Allocations
-      </NavLink>
+    <div style={{ margin: "0 300px" }}>
+      <div style={{ display: "flex" }}>
+        {/* <h3>NavBar Placeholder</h3> */}
+        <NavLink 
+          to="/"
+          exact
+          // style={({ isActive }) => ({
+          //   linkStyles,
+          //   background: isActive ? "grey" : null
+          // })}
+          style={({ isActive }) => (
+            isActive ? linkStylesActive : linkStyles 
+          )}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/favorites"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Favorites
+        </NavLink>
+        <NavLink 
+          to="/allocations"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Allocations
+        </NavLink>
+      </div>
     </div>
   )
 }
