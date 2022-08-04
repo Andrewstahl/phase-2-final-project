059fa512ebd08655c7ce568c56405f640e6841b4
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-04 19:02:36 -0400

    Changed name from 'StockForm' to 'NewOrderForm' for a more accurate description

---
+++ src/Components/NewOrderForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function NewOrderForm() {
+  const [userSelection, setUserSelection] = useState("Number")
+
+  return (
+    <div className="form-container">
+      <form>
+        <label htmlFor="stockTicker">Enter the Stock Ticker for this Stock</label>
+        <input 
+          type="text"
+          name="stockTicker" 
+          placeholder="Enter Stock Ticker"
+          />
+        <label htmlFor="buy-type-select">Enter Which Option You Are Looking to Buy</label>
+        <select name="buy-type-select" onChange={() => setUserSelection(userSelection === "Dollars" ? "Number" : "Dollars")}>
+          <option value="number">Number of Stocks</option>
+          <option value="dollars">Dollar Amount</option>
+        </select>
+        <label htmlFor="buyAmount">Enter The Buy Amount</label>
+        <input 
+          type="number" 
+          name="buyAmount"
+          placeholder={`Enter ${userSelection}`} 
+          />
+        <button>Check Current Price</button>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+  )
+}
